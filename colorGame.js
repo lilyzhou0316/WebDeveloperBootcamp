d7d0d1363129b6dc72696c5b24140c37ab480216
lilyzhou0316 <lilyzhou0316@hotmail.com>
2020-03-25 22:57:01 -0700

    first step

---
+++ colorGame.js
@@ -0,0 +1,38 @@
+/*
+ * @Author: Xiaoli Zhou
+ * @Date: 2020-03-25 22:24:00
+ * @LastEditTime: 2020-03-25 22:55:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /RGBRecognizerApp/colorGame.js
+ */
+//红，绿，蓝三原色, be careful with the space between comma and numbers
+var colors = ["rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+];
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colordisplay = document.querySelector("#colorDisplay");
+colordisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+    //add initial colors to squares
+    squares[i].style.backgroundColor = colors[i]
+
+    //add click event listeners to squares
+    squares[i].addEventListener("click", function () {
+        //grab color of clicked square
+        var currentColor = this.style.backgroundColor;
+        //compare it to the picked color
+        if (currentColor === pickedColor) {
+            alert("correct");
+        } else {
+            alert("wrong");
+        }
+    });
+};
