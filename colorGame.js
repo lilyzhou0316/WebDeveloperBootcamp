c401282543a7551d548e7c11b4caa5b1f43b34fe
lilyzhou0316 <lilyzhou0316@hotmail.com>
2020-03-25 23:06:00 -0700

    add some comments

--- colorGame.js
+++ colorGame.js
@@ -1,17 +1,17 @@
 /*
  * @Author: Xiaoli Zhou
  * @Date: 2020-03-25 22:24:00
- * @LastEditTime: 2020-03-25 22:55:16
+ * @LastEditTime: 2020-03-25 23:05:17
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /RGBRecognizerApp/colorGame.js
  */
 //红，绿，蓝三原色, be careful with the space between comma and numbers
-var colors = ["rgb(255, 0, 0)",
+var colors = ["rgb(255, 0, 0)", //red
     "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
+    "rgb(0, 255, 0)", //green
     "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
+    "rgb(0, 0, 255)", //blue
     "rgb(255, 0, 255)"
 ];
 
